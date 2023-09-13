be1db0c2646218189baba0cac8ab74e7e2fbe598
cylaubq <cylaubq@ust.hk>
2023-09-13 11:14:21 +0800

    Added 2 println as report heading

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -2,6 +2,8 @@ package Lab1;
 
 public class mainApp1 {
     public static void main(String[] args) {
+        System.out.println("Welcome to Scientific Calculator!");
+        System.out.println("Program started ...");
         int b = 2;
         int n = 11;
         System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
